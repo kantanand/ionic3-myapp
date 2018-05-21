bb7946ec091297f3b149ca1f545e9c15eeaaff3e
kantanand <kantanand.usk@gmail.com>
2018-05-22 00:04:19 +0530

    added gps support

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { Geolocation } from 'ionic-plugins';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -6,9 +7,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  coords: any;
+  accuracy: any;
+  error: any;
   constructor(public navCtrl: NavController) {
 
   }
+
+  watch() {
+    Geolocation.getCurrentPosition().then((resp) => {
+      this.coords = resp.coords.latitude + ' ' + resp.coords.longitude;
+      this.accuracy = resp.coords.accuracy + ' meters';
+    }).catch((error) => {
+      this.error = 'Error getting location: ' + error;
+    });
+  }
+
 
 }
