d4a471fcd7d8b5d570f21e079cc6604a660a84f2
kantanand <kantanand.usk@gmail.com>
2018-05-22 00:23:35 +0530

    fourth commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Geolocation } from 'ionic-plugins';
+// import { Geolocation } from 'ionic-plugins';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -10,17 +10,17 @@ export class HomePage {
   coords: any;
   accuracy: any;
   error: any;
-  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+  constructor(public navCtrl: NavController) {
 
   }
 
   watch() {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.coords = resp.coords.latitude + ' ' + resp.coords.longitude;
-      this.accuracy = resp.coords.accuracy + ' meters';
-    }).catch((error) => {
-      this.error = 'Error getting location: ' + error;
-    });
+    // this.geolocation.getCurrentPosition().then((resp) => {
+    //   this.coords = resp.coords.latitude + ' ' + resp.coords.longitude;
+    //   this.accuracy = resp.coords.accuracy + ' meters';
+    // }).catch((error) => {
+    //   this.error = 'Error getting location: ' + error;
+    // });
   }
 
 
